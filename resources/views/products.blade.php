f885a59ef521b93840711be5ac0662d057f78cbc
a.yergeshov <a.yergeshov@consaltica.ru>
2021-12-25 14:34:00 +0600

    products

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,10 +1,142 @@
 @extends('layouts.app-full')
 @section('content')
-   <section>
+    <section class="products d-flex align-items-end">
        <div class="products-text">
            <h1>Products</h1>
            <p>Product development is the complete process of delivering a new product or improving an existing one for customers.</p>
        </div>
-   </section>
-    <section></section>
+    </section>
+    <section>
+        <div class="products-second">
+            <div class="products-list">
+                <ul>
+                    <li>Industrial & Mechanical & Production Engineering</li>
+                    <li>Hardware Development & POC & MVP & Prototype</li>
+                    <li>Embeeded R&D & Robotic & Automotive & IOT</li>
+                    <li>Artificial Intelligence</li>
+                    <li>Embeeded R&D & Robotic & Automotive & IOT</li>
+                    <li>Artificial Intelligence </li>
+                    <li>Data Science </li>
+                    <li>Customer Experience Management</li>
+                </ul>
+            </div>
+            <div class="products-laptop">
+                <img src="{{url('assets/images/products/laptop.svg')}}" alt="laptop">
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="products-work">
+            <h1 class="products-title">Our Work</h1>
+            <div class="d-flex align-items-center justify-content-center flex-wrap">
+                <div class="work d-flex flex-column align-items-center justify-content-center">
+                    <img src="{{url('assets/images/products/work-background-1.jpg')}}" alt="">
+                    <div class="text-box">
+                        <h3>Full suite of digital marketing services
+                            for dementia support company</h3>
+                        <p>MEMORY MATTERS & MOMENTS CAFÉ</p>
+                    </div>
+                </div>
+                <div class="work d-flex flex-column align-items-center justify-content-center">
+                    <img src="{{url('assets/images/products/work-background-2.jpg')}}" alt="">
+                    <div class="text-box">
+                        <h3>Web design and support for
+                            Exeter based cancer charity</h3>
+                        <p>FORCE CANCER CHARITY</p>
+                    </div>
+                </div>
+                <div class="work d-flex flex-column align-items-center justify-content-center">
+                    <img src="{{url('assets/images/products/work-background-3.jpg')}}" alt="">
+                    <div class="text-box">
+                        <h3>Contemporary web design for North
+                            Devon Edwardian Hotel & Restaurant</h3>
+                        <p>THE WATERSMEET HOTEL & RESTAURANT</p>
+                    </div>
+                </div>
+                <div class="work d-flex flex-column align-items-center justify-content-center">
+                    <img src="{{url('assets/images/products/work-background-4.jpg')}}" alt="">
+                    <div class="text-box">
+                        <h3>Organic SEO and copywriting for
+                            leading window & door installers</h3>
+                        <p>MAJESTIC DESIGNS</p>
+                    </div>
+                </div>
+                <div class="work d-flex flex-column align-items-center justify-content-center">
+                    <img src="{{url('assets/images/products/work-background-5.jpg')}}" alt="">
+                    <div class="text-box">
+                        <h3>Organic SEO and copywriting for
+                            leading window & door installers</h3>
+                        <p>MAJESTIC DESIGNS</p>
+                    </div>
+                </div>
+                <div class="work d-flex flex-column align-items-center justify-content-center">
+                    <img src="{{url('assets/images/products/work-background-6.jpg')}}" alt="">
+                    <div class="text-box">
+                        <h3>Organic SEO and copywriting for
+                            leading window & door installers</h3>
+                        <p>MAJESTIC DESIGNS</p>
+                    </div>
+                </div>
+                <div class="work d-flex flex-column align-items-center justify-content-center">
+                    <img src="{{url('assets/images/products/work-background-7.jpg')}}" alt="">
+                    <div class="text-box">
+                        <h3>Organic SEO and copywriting for
+                            leading window & door installers</h3>
+                        <p>MAJESTIC DESIGNS</p>
+                    </div>
+                </div>
+                <div class="work d-flex flex-column align-items-center justify-content-center">
+                    <img src="{{url('assets/images/products/work-background-8.jpg')}}" alt="">
+                    <div class="text-box">
+                        <h3>Organic SEO and copywriting for
+                            leading window & door installers</h3>
+                        <p>MAJESTIC DESIGNS</p>
+                    </div>
+                </div>
+                <div class="work d-flex flex-column align-items-center justify-content-center">
+                    <img src="{{url('assets/images/products/work-background-9.jpg')}}" alt="">
+                    <div class="text-box">
+                        <h3>Organic SEO and copywriting for
+                            leading window & door installers</h3>
+                        <p>MAJESTIC DESIGNS</p>
+                    </div>
+                </div>
+                <div class="work d-flex flex-column align-items-center justify-content-center">
+                    <img src="{{url('assets/images/products/work-background-10.jpg')}}" alt="">
+                    <div class="text-box">
+                        <h3>Organic SEO and copywriting for
+                            leading window & door installers</h3>
+                        <p>MAJESTIC DESIGNS</p>
+                    </div>
+                </div>
+                <div class="work d-flex flex-column align-items-center justify-content-center">
+                    <img src="{{url('assets/images/products/work-background-11.jpg')}}" alt="">
+                    <div class="text-box">
+                        <h3>Organic SEO and copywriting for
+                            leading window & door installers</h3>
+                        <p>MAJESTIC DESIGNS</p>
+                    </div>
+                </div>
+                <div class="work d-flex flex-column align-items-center justify-content-center">
+                    <img src="{{url('assets/images/products/work-background-12.jpg')}}" alt="">
+                    <div class="text-box">
+                        <h3>Organic SEO and copywriting for
+                            leading window & door installers</h3>
+                        <p>MAJESTIC DESIGNS</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="products-business">
+            <h1> Build your business with us!</h1>
+            <p>IT operational risk refers to any threat that could lead to the inappropriate modification, lack of availability, destruction, or theft of IT assets.
+                IT operational risk refers to any threat that could lead to the inappropriate modification, lack of availability, destruction, or theft of IT assets. To oversee IT risk, boards must understand the risks technology poses
+                to the institution, and have questions for management that drive a
+                real understanding of the risk landscape and set clear direction and
+                expectations
+            </p>
+        </div>
+    </section>
 @endsection
