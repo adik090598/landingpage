867d0a373236265879d230b9078d564e6c655a43
a.yergeshov <a.yergeshov@consaltica.ru>
2021-12-25 19:52:19 +0600

    подключил новую страницу about and vacancies

---
+++ resources/views/Vacancies.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app-full')
+@section('content')
+    <section>
+        <div class="vacancies d-flex align-items-center justify-content-center">
+            <div class="vacancies-text">
+                <h1>We have 15 openings! <br>
+                    Find one that suits your career!</h1>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="find-jobs">
+            <h1>Find the best jobs</h1>
+            <div class="list-jobs">
+                <ul>
+                    <li><a href="">Senior Infrastructure Architect - Remote |LATAM</a>
+                    <button type="button" class="list-button">Apply<icon></icon></button>
+                    </li>
+                    <li><a href="">Job Boards Specialists - Remote</a></li>
+                    <li><a href="">Product Manager - Remote</a></li>
+                    <li><a href="">QA Analyst Senior - Remote</a></li>
+                    <li><a href="">Social Media Manager - Remote</a></li>
+                    <li><a href="">Business Development Operations Analyst - Remote</a></li>
+                </ul>
+            </div>
+            <hr>
+        </div>
+    </section>
+    <section>
+        <div class="vacancies-last">
+            <div class="vacancies-last-title">
+                <h1>Make the great future with our company. <br>
+                    Become a part of our family!</h1>
+            </div>
+        </div>
+    </section>
+@endsection
