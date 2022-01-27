615ddd0b60c2ceb0fd1cd46ebfaa0b398f83449f
adik090598 <adok09051998@gmail.com>
2022-01-27 17:22:38 +0600

    Navigation locale texts

--- resources/views/parts/navigation.blade.php
+++ resources/views/parts/navigation.blade.php
@@ -1,10 +1,10 @@
 <nav>
     <ul class="d-none d-md-block">
-        <li><a class="nav-a active" href="/">Home</a></li>
-        <li hidden><a class="nav-a" href="/products">Products</a></li>
-        <li><a class="nav-a" href="/outsourcing">IT Outsourcing Services</a></li>
-        <li><a class="nav-a" href="/about">About us</a></li>
-        <li><a class="nav-a" href="/vacancies">Vacancies</a></li>
+        <li><a class="nav-a active" href="/">{{app()->getLocale() == 'en' ? 'Home' : 'Главная'}}</a></li>
+        <li hidden><a class="nav-a" href="/products">{{app()->getLocale() == 'en' ? 'Products' : 'Продукты'}}</a></li>
+        <li><a class="nav-a" href="/outsourcing">{{app()->getLocale() == 'en' ? 'IT Outsourcing Services' : 'Услуги ИТ-аутсорсинга'}}</a></li>
+        <li><a class="nav-a" href="/about">{{app()->getLocale() == 'en' ? 'About us' : 'О нас'}}</a></li>
+        <li><a class="nav-a" href="/vacancies">{{app()->getLocale() == 'en' ? 'Vacancies' : 'Вакансии'}}</a></li>
         <li><a class="nav-a" href="{{route('locale', ['locale'=>'en'])}}">
                 @if(app()->getLocale()=='en')
                     <b>Eng</b>
